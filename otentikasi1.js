2b2c8bc80a7eef8de544f1098f2a85c6a647eb80
Aziz Maulana Hakim <azizmh.0809@gmail.com>
2024-05-07 21:19:25 +0700

    First Commit

---
+++ otentikasi1.js
@@ -0,0 +1,12 @@
+function validateLogin() {
+
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    if (username === 'adminpuskesmas' & password === 'admin2024') {
+        alert('Login berhasil');
+        window.location.href = 'admin.php'; 
+    } else {
+        alert('Login Gagal. (Username = adminpuskesmas | Password = admin2024).');
+    }
+}
